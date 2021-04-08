fd4dee63e1469cd2d7ddd7caa98e3feb5b384928
bassfault <herbert71@hotmail.it>
2021-04-08 16:36:40 +0200

    Testing a new scratch implementation of a sparse set

--- sparse.c
+++ sparse.c
@@ -2,65 +2,67 @@
 
 #include <stdlib.h>
 
-// enum
-// {
-//     INDEX_BITS = 24,
-//     GENERATION_BITS = 8,
-// };
-
-// typedef u32 Entity;
-
-// typedef union EntityIndex
-// {
-//     u32 index : INDEX_BITS;
-//     u32 generation : GENERATION_BITS;
-// } EntityIndex;
-
-// STATIC_ASSERT((INDEX_BITS + GENERATION_BITS) / 8 == sizeof(EntityIndex), "Wrong entity size");
-
-typedef struct Registry
+enum
 {
-    u32 *index;
-    u32 size;
-} Registry;
+    MAX_INDICES = 1024
+};
 
-typedef struct Store
+typedef struct Sparse
 {
-    u32 *index;
-    u8 *data;
-    u32 cap;
-    u32 len;
-    u32 data_size;
-} Store;
+    u32 sparse[MAX_INDICES];
+    u32 dense[MAX_INDICES];
 
-void
-registry_init(Registry *reg, u32 size)
-{
-    reg->index = calloc(size, sizeof(*reg->index));
-    reg->size = size;
-}
-
-void
-store_init(Store *store, Registry const *reg, u32 data_size)
-{
-    store->len = 0;
-    store->cap = reg->size;
-    store->index = malloc(store->cap * sizeof(*store->index));
-    store->data = malloc(store->cap * data_size);
-    store->data_size = data_size;
-
-    for (u32 i = 0; i < store->cap; ++i)
-    {
-        store->index[i] = store->cap;
-    }
-}
+    u32 next;
+    u32 count;
+} Sparse;
 
 bool
-store_add(Store *store, u32 index, void const *data)
+sparse_has(Sparse const *sparse, u32 index)
 {
-    if (index >= store->cap) return false;
+    u32 dense_index = sparse->sparse[index];
+    return dense_index < sparse->count && sparse->dense[dense_index] == index;
+}
 
-    if (store->index[index] == store->cap)
+u32
+sparse_add(Sparse *sparse)
+{
+    sparse->dense[sparse->count] = sparse->next;
+    sparse->sparse[sparse->next] = sparse->count++;
+
+    return sparse->next++;
+}
+
+void
+sparse_remove(Sparse *sparse, u32 index)
+{
+    if (sparse->count < 1) return;
+
+    u32 dense_index = sparse->sparse[index];
+
+    if (index < sparse->count && sparse->dense[dense_index] == index)
     {
+        u32 sparse_index = sparse->dense[--sparse->count];
+        sparse->sparse[sparse_index] = dense_index;
+        sparse->dense[dense_index] = sparse_index;
     }
+}
+
+i32
+main(void)
+{
+    Sparse sparse = {0};
+
+    u32 a = sparse_add(&sparse);
+
+    ASSERT(sparse_has(&sparse, a), "");
+
+    u32 b = sparse_add(&sparse);
+
+    ASSERT(sparse_has(&sparse, b), "");
+
+    sparse_remove(&sparse, a);
+
+    ASSERT(!sparse_has(&sparse, a), "");
+
+    return 0;
 }
