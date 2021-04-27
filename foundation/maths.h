62b0e26deefbcdda7010befcf2ea9b486e43e0dc
bassfault <herbert71@hotmail.it>
2021-04-27 16:13:42 +0200

    Added namespacing to the math utility functions

--- foundation/maths.h
+++ foundation/maths.h
@@ -4,16 +4,32 @@
 
 #include "common.h"
 
-#define cos(X) _Generic((X), default : cos, f32 : cosf)(X)
-#define sin(X) _Generic((X), default : sin, f32 : cosf)(X)
+#define cfCos(X) _Generic((X), default : cos, f32 : cosf)(X)
+#define cfSin(X) _Generic((X), default : sin, f32 : cosf)(cfS)
 
-#define sqrt(X) _Generic((X), default : sqrt, f32 : sqrtf)(X)
+#define cfSqrt(X) _Generic((X), default : sqrt, f32 : sqrtf)(X)
 
-#define ceil(X) _Generic((X), default : ceil, f32 : ceilf)(X)
-#define floor(X) _Generic((X), default : floor, f32 : floorf)(X)
-#define round(X) _Generic((X), default : round, f32 : roundf)(X)
+#define cfPow(base, exp) _Generic((base, exp), default : pow, f32 : powf)(base, exp)
 
-#define fmod(X, Y) _Generic((X, Y), default : fmod, f32 : fmodf)(X, Y)
+#define cfCeil(X) _Generic((X), default : ceil, f32 : ceilf)(X)
+#define cfFloor(X) _Generic((X), default : floor, f32 : floorf)(X)
+#define cfRound(X) _Generic((X), default : round, f32 : roundf)(X)
+
+#define cfFmod(X, Y) _Generic((X, Y), default : fmod, f32 : fmodf)(X, Y)
+
+#define cfLerp(x, y, t) _Generic((x, y, t), default : cf__Lerp64, f32 : cf__Lerp32)(x, y, t)
+
+static inline f32
+cf__Lerp32(f32 x, f32 y, f32 t)
+{
+    return x * (1 - t) + y * t;
+}
+
+static inline f64
+cf__Lerp64(f64 x, f64 y, f64 t)
+{
+    return x * (1 - t) + y * t;
+}
 
 #define CF_MATHS_H
 #endif
