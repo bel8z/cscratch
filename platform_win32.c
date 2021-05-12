c53684bb64e59a902fad61c372fb7c8f1c6e7852
bassfault <herbert71@hotmail.it>
2021-05-12 10:41:07 +0200

    Reducing Win32 specific inclusions

--- platform_win32.c
+++ platform_win32.c
@@ -9,9 +9,14 @@
 #pragma warning(push)
 #pragma warning(disable : 5105)
 
-#include <windows.h>
-
 #include <stdio.h>
+
+#define NOMINMAX 1
+#define VC_EXTRALEAN 1
+#define WIN32_LEAN_AND_MEAN 1
+#include <windows.h>
+// Must be included AFTER <windows.h>
+#include <commdlg.h>
 
 //------------------------------------------------------------------------------
 // Internal function declarations
