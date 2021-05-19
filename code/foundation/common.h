ba431d977dac12173c6d273731114878417e6bd8
bassfault <herbert71@hotmail.it>
2021-05-19 11:43:58 +0200

    Assert management review

--- code/foundation/common.h
+++ code/foundation/common.h
@@ -9,6 +9,12 @@
 #define CF_MEMORY_PROTECTION 1
 #endif
 
+// NOTE (Matteo): Asserts in release builds are enabled by default, and can be disabled as a
+// compilation flag
+#if !defined(CF_RELEASE_ASSERTS)
+#define CF_RELEASE_ASSERTS 1
+#endif
+
 //------------------------------------------------------------------------------
 // Alignment
 
@@ -196,25 +202,28 @@ union Hsva
 //------------------------------------------------------------------------------
 // Assertion macros
 
-// Ensure assertion in release mode
-#define CF_RELEASE_ASSERTS 1
-
-#if defined(CF_RELEASE_ASSERTS) && defined(NDEBUG)
-#define CF__RESTORE_NDEBUG
+#if CF_RELEASE_ASSERTS && defined(NDEBUG)
+#define CF__RESTORE_NDEBUG 1
 #undef NDEBUG
 #endif
 
 #include <assert.h>
 
 #define CF_STATIC_ASSERT(expr, msg) _Static_assert(expr, msg)
+
+#if defined(NDEBUG)
+#define CF_ASSERT(expr, msg) (CF_UNUSED(expr), CF_UNUSED(msg))
+#else
 #define CF_ASSERT(expr, msg) (assert((expr) && (msg)))
+#endif
+
 #define CF_ASSERT_NOT_NULL(ptr) CF_ASSERT(ptr, #ptr " is null")
 
 #define CF_NOT_IMPLEMENTED() CF_ASSERT(false, "Not implemented")
 
 #define CF_INVALID_CODE_PATH() CF_ASSERT(false, "Invalid code path")
 
-#if defined(RESTORE_NDEBUG)
+#if defined(CF__RESTORE_NDEBUG)
 #define NDEBUG 1
 #undef CF__RESTORE_NDEBUG
 #endif
