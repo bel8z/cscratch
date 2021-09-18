4970df628142e23c246bed3d0bafb2bd313ace35
bassfault <herbert71@hotmail.it>
2021-09-18 17:39:32 +0200

    Implemented atomics using clang c11 builtins

--- code/foundation/atom.h
+++ code/foundation/atom.h
@@ -8,10 +8,35 @@
 #    define ATOM_ENSURE_ALIGN(decl, amt) decl __attribute__((aligned(amt)))
 #endif
 
+#if CF_COMPILER_CLANG && __has_builtin(__c11_atomic_init)
+#    define ATOM__CLANG_BUILTIN 1
+#else
+#    define ATOM__CLANG_BUILTIN 0
+#endif
+
+//----------------------------------------------------------------------------//
+// Types
+
+#if ATOM__CLANG_BUILTIN
+
+typedef _Atomic(I8) AtomI8;
+typedef _Atomic(U8) AtomU8;
+typedef _Atomic(I16) AtomI16;
+typedef _Atomic(U16) AtomU16;
+typedef _Atomic(I32) AtomI32;
+typedef _Atomic(U32) AtomU32;
+typedef _Atomic(I64) AtomI64;
+typedef _Atomic(U64) AtomU64;
+typedef _Atomic(Isize) AtomIsize;
+typedef _Atomic(Usize) AtomUsize;
+typedef _Atomic(void *) AtomPtr;
+
+#else
+
 // clang-format off
+ATOM_ENSURE_ALIGN(typedef struct AtomI32 { I32  volatile  inner; } AtomI32, 4);
                   typedef struct AtomI8  { I8   volatile  inner; } AtomI8;
 ATOM_ENSURE_ALIGN(typedef struct AtomI16 { I16  volatile  inner; } AtomI16, 2);
-ATOM_ENSURE_ALIGN(typedef struct AtomI32 { I32  volatile  inner; } AtomI32, 4);
 ATOM_ENSURE_ALIGN(typedef struct AtomI64 { I64  volatile  inner; } AtomI64, 8);
                   typedef struct AtomU8  { U8   volatile  inner; } AtomU8;
 ATOM_ENSURE_ALIGN(typedef struct AtomU16 { U16  volatile  inner; } AtomU16, 2);
@@ -20,26 +45,33 @@ ATOM_ENSURE_ALIGN(typedef struct AtomU64 { U64  volatile  inner; } AtomU64, 8);
 ATOM_ENSURE_ALIGN(typedef struct AtomPtr { void volatile *inner; } AtomPtr, CF_PTR_SIZE);
 // clang-format on
 
-#if CF_PTR_SIZE == 4
+#    if CF_PTR_SIZE == 4
 
 CF_STATIC_ASSERT(sizeof(Isize) == 4 && sizeof(Usize) == 4, "Invalid size types");
 
 typedef AtomI32 AtomIsize;
 typedef AtomU32 AtomUsize;
 
-#elif CF_PTR_SIZE == 8
+#    elif CF_PTR_SIZE == 8
 
 CF_STATIC_ASSERT(sizeof(Isize) == 8 && sizeof(Usize) == 8, "Invalid size types");
 
 typedef AtomI64 AtomIsize;
 typedef AtomU64 AtomUsize;
 
-#else
-#    error "Unsupported pointer size"
+#    else
+#        error "Unsupported pointer size"
+#    endif
+
 #endif
 
 //----------------------------------------------------------------------------//
+// Common read-write operations
 
+#if ATOM__CLANG_BUILTIN
+#    define atomRead(value) __c11_atomic_load(value, __ATOMIC_RELAXED)
+#    define atomWrite(object, value) __c11_atomic_store(object, value, __ATOMIC_RELAXED)
+#else
 // clang-format off
 #define atomRead(x)                     \
     _Generic((x),                       \
@@ -65,10 +97,53 @@ typedef AtomU64 AtomUsize;
              AtomU64* : atomWrite64(x, y),  \
              AtomPtr* : (x)->inner = y)
 // clang-format on
+#endif // ATOM__CLANG_BUILTIN
 
 //----------------------------------------------------------------------------//
+// Platform specific built-ins
 
-#if CF_OS_WIN32
+#if ATOM__CLANG_BUILTIN
+//-----------------------//
+//   CPU memory fences   //
+//-----------------------//
+
+#    define atomAcquireFence() __c11_atomic_thread_fence(__ATOMIC_ACQUIRE)
+#    define atomReleaseFence() __c11_atomic_thread_fence(__ATOMIC_RELEASE)
+#    define atomSequentialFence() __c11_atomic_thread_fence(__ATOMIC_SEQ_CST)
+
+//---------------------//
+//   Compiler fences   //
+//---------------------//
+
+#    define atomAcquireCompFence() __c11_atomic_signal_fence(__ATOMIC_ACQUIRE)
+#    define atomReleaseCompFence() __c11_atomic_signal_fence(__ATOMIC_RELEASE)
+#    define atomSequentialCompFence() __c11_atomic_signal_fence(__ATOMIC_SEQ_CST)
+
+//-----------------------//
+//   Atomic operations   //
+//-----------------------//
+
+#    define atomCompareExchange(value, expected, desired)                                \
+        __c11_atomic_compare_exchange_strong(value, expected, desired, __ATOMIC_RELAXED, \
+                                             __ATOMIC_RELAXED)
+
+#    define atomExchange(value, desired) __c11_atomic_exchange(value, desired, __ATOMIC_RELAXED)
+
+#    define atomFetchAnd(value, operand) __c11_atomic_fetch_and(value, operand, __ATOMIC_RELAXED)
+
+#    define atomFetchOr(value, operand) __c11_atomic_fetch_or(value, operand, __ATOMIC_RELAXED)
+
+#    define atomFetchAdd(value, operand) __c11_atomic_fetch_add(value, operand, __ATOMIC_RELAXED)
+
+#    define atomFetchSub(value, operand) __c11_atomic_fetch_sub(value, operand, __ATOMIC_RELAXED)
+
+#    define atomFetchInc(value) atomFetchAdd(value, 1)
+
+#    define atomFetchDec(value) atomFetchSub(value, 1)
+
+// ATOM__CLANG_BUILTIN
+//----------------------------------------------------------------------------//
+#elif CF_OS_WIN32
 
 #    include <intrin.h>
 
@@ -84,6 +159,7 @@ typedef AtomU64 AtomUsize;
         _InterlockedAnd64, _InterlockedOr8, _InterlockedOr16, _InterlockedOr, _InterlockedOr64)
 
 #    if CF_COMPILER_CLANG
+
 #        define ATOM__UNDEPRECATE(intrin)                                         \
             _Pragma("clang diagnostic push")                                      \
                 _Pragma("clang diagnostic ignored \"-Wdeprecated-declarations\"") \
@@ -115,6 +191,7 @@ typedef AtomU64 AtomUsize;
 // TODO (Matteo): Restore pointer operations
 
 // clang-format off
+
 #define atomCompareExchange(value, expected, desired)                                                   \
     _Generic((value),                                                                                   \
              AtomI8 * : _InterlockedCompareExchange8      ((char volatile*)(value), desired, expected), \
@@ -125,6 +202,7 @@ typedef AtomU64 AtomUsize;
              AtomU16* : _InterlockedCompareExchange16     ((I16  volatile*)(value), desired, expected), \
              AtomU32* : _InterlockedCompareExchange       ((long volatile*)(value), desired, expected), \
              AtomU64* : _InterlockedCompareExchange64     ((I64  volatile*)(value), desired, expected))
+
 
 #define atomExchange(value, desired)                                                    \
     _Generic((value),                                                                   \
@@ -283,8 +361,8 @@ atom__Write64(U64 volatile *object, U64 value)
 
 #    endif
 
+// CF_OS_WIN32
 //----------------------------------------------------------------------------//
-
 #else
 #    error "Atomics not yet supported on this platform"
 #endif
