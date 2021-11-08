25cd8ee28dd980fdebec6d31a3b665f07a96ed7d
bassfault <herbert71@hotmail.it>
2021-11-08 09:32:09 +0100

    Vulkan test: added custom font loading

--- code/vulkan_test/vulkan_test.c
+++ code/vulkan_test/vulkan_test.c
@@ -857,21 +857,9 @@ appInitGui(App *app, Platform *platform)
     // Setup Dear ImGui style
     guiSetupStyle(GuiTheme_Dark, dpi_scale);
 
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can
-    // also load multiple fonts and use igPushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
-    // need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please
-    // handle those errors in your application (e.g. use an assertion, or
-    // display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and
-    // stored into a texture when calling
-    // ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame
-    // below will call.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string
-    // literal you need to write a double backslash \\ !
+    // Setup Dear ImGui fonts
+    ImFontAtlas *fonts = guiFonts();
+    if (!guiLoadCustomFonts(fonts, dpi_scale, paths->data)) guiLoadDefaultFont(fonts);
 
     ImGui_ImplGlfw_InitForVulkan(app->window, true);
 }
